ff674cf01376cccd276aed618dc457807ef13f04
tatjam <tatjam02@gmail.com>
2026-08-30 11:28:17 +0200

    Add the quadrature

--- code/src/propagators/PolyChaos.jl
+++ code/src/propagators/PolyChaos.jl
@@ -103,7 +103,8 @@ and thus we get a series of equations for each β
     cᵦ = <f, Ψᵦ>
 
 Finally, we evaluate the expected value via the quadrature rule, which is chosen so it's highly accurate for
-our distribution (in fact, ideally should be exact if f is a linear combination of the basis functions).
+our distribution (in fact, ideally should be exact if f is a linear combination of the basis functions). Note that
+if high performance is desired, precomputation of the quadrature will save some effort.
 """
 function galerkin(f, b::AbstractPCEBasis, q::AbstractPCEQuadrature=quadrature(b))
     ns, ws = nodes(q), weights(q)
@@ -143,33 +144,33 @@ We store N terms, but the 0th term is implicit, and of value 1.0,
 thus the basis represents polynomials of order [0, N] with both endpoints
 included.
 """
-struct OprlBasis{T<:AbstractFloat, N} <: Basis
+struct OprlBasis{T<:AbstractFloat,N} <: Basis
     # This is the Jacobi matrix of the recurrence relation compactly stored,
     # jacobi[i] yields the Favard terms c_{i-1} and sqrt(d_{i}) in that order. Remember that arrays
     # in Julia are 1-indexed! The square root is for normalization.
-    jacobi::SVector{N, Tuple{T,T}}
+    jacobi::SVector{N,Tuple{T,T}}
 end
 
 nvars(b::OprlBasis) = 1
-multi_index(b::OprlBasis{T, N}) where {T, N} = 0:N
+multi_index(b::OprlBasis{T,N}) where {T,N} = 0:N
 
-function eval_basis!(out::AbstractVector{T}, b::OprlBasis{T, N}, ξ) where {T, N}
+function eval_basis!(out::AbstractVector{T}, b::OprlBasis{T,N}, ξ) where {T,N}
     # The Favard form is just
-    #   y_{n+1} = (x - cₙ) yₙ - dₙ y_{n-1}
+    #   yₙ₊₁= (x - cₙ) yₙ - dₙ yₙ₋₁
     # Let hₙ = <yₙ, yₙ>, we wish to construct instead the sequence
     #   pₙ = yₙ / sqrt(hₙ)
     # which is orthonormal. Now, note that Favard sequences satisfy
-    #   <yₙ, yₙ> = dₙ <y_{n-1}, y_{n-1}>
-    # thus hₙ = dₙ h_{n-1}
-    # Now consider p_{n+1},
-    #  p_{n+1} = y_{n+1} / (sqrt(d_{n+1} h_n)
-    #          = [(x - cₙ) yₙ / sqrt(hₙ) - dₙ y_{n-1} / sqrt(hₙ)] / sqrt(d_{n+1})
-    #          = [(x - cₙ) pₙ            - dₙ y_{n-1} / sqrt(dₙ h_{n-1})] / sqrt(d_{n+1})
-    #          = [(x - cₙ) pₙ - sqrt(dₙ) p_{n-1}] / sqrt(d_{n+1})
+    #   <yₙ, yₙ> = dₙ <yₙ₋₁, yₙ₋₁>
+    # thus hₙ = dₙ hₙ₋₁
+    # Now consider pₙ₊₁
+    #  pₙ₊₁= yₙ₊₁/ (sqrt(dₙ₊₁hₙ)
+    #          = [(x - cₙ) yₙ / sqrt(hₙ) - dₙ yₙ₋₁ / sqrt(hₙ)] / sqrt(dₙ₊₁)
+    #          = [(x - cₙ) pₙ            - dₙ yₙ₋₁ / sqrt(dₙ hₙ₋₁)] / sqrt(dₙ₊₁)
+    #          = [(x - cₙ) pₙ - sqrt(dₙ) pₙ₋₁] / sqrt(dₙ₊₁)
 
     # y₀ = p₀ = 1
     out[1] = one(T)
-    
+
     # y₁ = (x - c₀) y₀, thus
     # sqrt(d₁) p₁ = (x - c₀) p₀
     if N >= 1
@@ -177,18 +178,61 @@ function eval_basis!(out::AbstractVector{T}, b::OprlBasis{T, N}, ξ) where {T, N
         out[2] = ((ξ - c0) * out[1]) / sqd1
     end
 
-    # y_{n+1} = (x - cₙ) yₙ - dₙ y_{n-1}, thus
-    # sqrt(d_{n+1}) p_{n+1} = (x - cₙ) pₙ - sqrt(dₙ) p_{n-1}
+    # yₙ₊₁= (x - cₙ) yₙ - dₙ yₙ₋₁, thus
+    # sqrt(dₙ₊₁ pₙ₊₁= (x - cₙ) pₙ - sqrt(dₙ) pₙ₋₁
     for n in 1:(N-1)
         cn, sqdnp1 = b.jacobi[n+1]
         _, sqdn = b.jacobi[n]
 
         # the +1 due to 1 based indexing
-        out[(n+1) + 1] = ((ξ - cn) * out[n + 1] - sqdn * out[(n-1) + 1]) / sqdnp1
+        out[(n+1)+1] = ((ξ - cn) * out[n+1] - sqdn * out[(n-1)+1]) / sqdnp1
     end
 
     return out
 end
+
+struct OprlQuadrature{T<:AbstractFloat,N}
+    nodes::SVector{N,T}
+    weights::SVector{N,T}
+end
+
+function quadrature(b::OprlBasis{T,N}) where {T,N}
+    # Note the Jacobi matrix is written by Golub and Welsch as
+    #   yₙ = (aₙx + bₙ)yₙ₋₁ - cₙyₙ₋₂
+    #   [-b₁/a₁   1/a₁                   ...
+    #   [c₂/a₂    -b₂/a₂   1/a₂          ...
+    #   [         c₃/a₃    -b₃/a₃   1/a₃ ...
+    #
+    # But our recurence relation is
+    #  pₙ₊₁= [(x - cₙ) pₙ - sqrt(dₙ) pₙ₋₁] / sqrt(dₙ₊₁)
+    #
+    # Identifying terms (see writeup), we write the Jacobi matrix as
+    #   [c₀   √d₁       ...
+    #   [√d₁  c₁   √d₂  ...
+    #   [     √d₂  c₃   ...
+
+    # The diagonal is all the first terms of jacobi, the off-diagonal the second ones,
+    # note that as expected there's one "unused" element in the jacobi array
+    jacobimat = SymTridiagonal(first.(b.jacobi), second.(b.jacobi)[1:end-1])
+
+    eigdecomp = eigen(jacobimat)
+    eigval = eigdecomp.values
+    eigvec = eigdecomp.vectors
+
+    # By Golub and Welsch (Calculation of Gauss quadrature rules, 1969), the
+    # nodes of the quadrature are the eigenvalues of the Jacobi matrix:
+    nodes = eigval
+    # and the weights are simply the first coordinates of each eigen vector squared
+    # (multiplied by the total mass of the distribution, but by definition that's 1)
+    weights = eigvec[1, :] .^ 2
+
+    return OprlQuadrature(nodes, weights)
+end
+
+
+nodes(q::OprlQuadrature) = q.nodes
+weights(q::OprlQuadrature) = q.weights
+
 
 """
 A polynomial basis orthogonal to some distribution on the unit circle,
